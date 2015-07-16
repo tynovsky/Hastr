353cc93e6d2adbab32fbc5e85b9393820d8533e4
Miroslav Tynovsky <tynovsky@avast.com>
2015-07-16 17:58:15 +0200

    refactoring + change random file's backup

--- lib/Hastr/File.pm
+++ lib/Hastr/File.pm
@@ -2,54 +2,64 @@ package Hastr::File;
 use 5.008001;
 use strict;
 use warnings;
-use Path::Tiny qw(path);
+use Path::Tiny qw();
 
 sub new {
     my ($class, %args) = @_;
     return bless \%args, $class;
 }
 
-sub root {
-    return shift->{root}
+sub random_from_backup_node {
+    my ($class, $backup_node, %args) = @_;
+
+    my $self = $class->new(%args);
+
+    my $path = glob($self->backups . '/' . $backup_node);
+    for (1 .. 4) { #4 levels deep = 3 levels of dirs, then 1 file
+        my @files = glob("$path/*");
+        $path     = $files[rand @files];
+    }
+
+    $self->{hash} = Path::Tiny::path($path)->basename =~ s/[.]dat$//r
+
+    return $self;
 }
 
-sub hash {
-    return shift->{hash}
-}
-
-sub upload {
-    return shift->{upload}
-}
+sub root    { shift->{root}    }
+sub backups { shift->{backups} }
+sub hash    { shift->{hash}    }
 
 sub name {
     return (shift->{hash} =~ s{^(..)(..)(..)}{$1/$2/$3/$1$2$3}r) . '.dat';
 }
 
-sub filepath {
+sub path {
     my ($self) = @_;
 
-    return path($self->{root} . '/' . $self->name);
+    return Path::Tiny::path($self->root . '/' . $self->name)
 }
 
 sub backup_path {
     my ($self, $node) = @_;
 
-    return path($self->{backups} . '/' . $node . '/' . $self->name);
+    return Path::Tiny::path($self->backups . "/$node/" . $self->name)
 }
 
 sub write {
+    my ($self, $upload, $backup_node) = @_;
+
+    $self->filepath->parent->mkpath();
+    $upload->move_to($self->filepath);
+    $self->create_backup_symlink($backup_node);
+}
+
+sub create_backup_symlink {
     my ($self, $backup_node) = @_;
 
-    my $filepath    = $self->filepath();
     my $backup_path = $self->backup_path($backup_node);
-
-    $filepath->parent->mkpath();
-    $self->upload->move_to($filepath);
-
     $backup_path->parent->mkpath();
-    symlink($filepath, $backup_path)
+    symlink($self->filepath, $backup_path)
         or die "Failed to create symlink $backup_path: $!";
-
 }
 
 sub delete {
@@ -65,3 +75,5 @@ sub exists {
 }
 
 1;
+
+__END__

dc0a48ba76de3233dfe240d95dbed8637915b027
Miroslav Tynovsky <tynovsky@avast.com>
2015-07-16 18:41:37 +0200

    Hastr client + server uses the client for calls

---
+++ lib/Hastr/Client.pm
@@ -0,0 +1,63 @@
+package Hastr::Client;
+use 5.008001;
+use strict;
+use warnings;
+use Mojo::UserAgent;
+use Mojo::Asset::File;
+
+our $VERSION = '0.1.0';
+
+sub new {
+    my ($class, $args) = @_;
+    my %args = %$args;
+    $args{ua} = Mojo::UserAgent->new(
+        max_connecions => delete $args{max_connections} // 10,
+        max_redirects  => delete $args{max_redirectes}  // 10,
+    );
+    return bless \%args, $class
+}
+
+sub post_file {
+    my ($self, %args) = @_;
+    #args: node, hash, (asset|path), [backup_node]
+
+    if (!exists $args{asset}) {
+        $args{asset} = Mojo::Asset::File->new(path => $args{path});
+    }
+
+    my $query_string = exists $args{backup_node}
+        ? "?backup-node=$args{backup_node}"
+        : '';
+
+    my $tx = $self->{ua}->post(
+        "http://$args{node}/file/$args{hash}$query_string"
+        => form
+        => { file => { file => $args{asset} } }
+    );
+
+    return $tx->res
+}
+
+sub get_file {
+    my ($self, $node, $hash) = @_;
+
+    my $tx = $self->{ua}->get("http://$node/file/$hash");
+
+    return $tx->res
+)
+
+sub exists_file {
+    my ($self, $node, $name) = @_;
+
+    my $tx = $self->{ua}->head("$node/$name");
+
+    return $tx->res->code == 200
+}
+
+sub delete_file {
+    my ($self, $node, $hash, $backup_node) = @_;
+
+    my $tx = $self->{ua}->delete("$node/file/$hash?backup-node=$backup_node");
+
+    return $tx->res;
+}
